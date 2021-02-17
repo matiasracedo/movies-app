435611d762947e7307fa6565da92108d13ba6334
matiasracedo <matiasracedo@gmail.com>
2021-02-17 20:57:03 -0300

    final?

---
+++ public/precache-manifest.2a2d97b1f81b56ef0036d1ac51addd7d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c0bf68ded466ab9dfe388dac4a5d164",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aafc2f1b8e3f1a49cfc1",
+    "url": "/static/css/main.d9dbbd92.chunk.css"
+  },
+  {
+    "revision": "26ca565f7f660dcdf5cf",
+    "url": "/static/js/2.0e9bd89d.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.0e9bd89d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aafc2f1b8e3f1a49cfc1",
+    "url": "/static/js/main.8f676b6f.chunk.js"
+  },
+  {
+    "revision": "67147a78ec849492f1cb",
+    "url": "/static/js/runtime-main.26567517.js"
+  }
+]);
