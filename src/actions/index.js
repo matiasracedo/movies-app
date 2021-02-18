564b085e63d102dc99c6ad535e98026559ac825d
matiasracedo <matiasracedo@gmail.com>
2021-02-17 21:22:54 -0300

    fixing permissions

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,7 +2,7 @@
  
 export function getMovies(titulo) {
     return function(dispatch) {
-      return fetch("http://www.omdbapi.com/?apikey=20dac387&s=" + titulo)
+      return fetch("https://www.omdbapi.com/?apikey=20dac387&s=" + titulo)
         .then(response => response.json())
         .then(json => {
           dispatch({ type: "GET_MOVIES", payload: json });
@@ -16,7 +16,7 @@ export function addMovieFavorite(payload) {
  
 export function getMovieDetail(id) {
     return function(dispatch) {
-        return fetch("http://www.omdbapi.com/?apikey=20dac387&i=" + id + "&plot=full")
+        return fetch("https://www.omdbapi.com/?apikey=20dac387&i=" + id + "&plot=full")
             .then(response => response.json())
             .then(json => {
                 dispatch({ type: "GET_MOVIE_DETAIL", payload: json });
